0ecb54034965a24e2b9ae629a5fa0491b41d28f4
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-11-20 15:02:05 -0500

    initial revision

---
+++ main.py
@@ -0,0 +1,86 @@
+import dataclasses
+import json
+import os
+import sys
+from pprint import pp
+from typing import List, Dict
+import pandas as pd
+
+
+# @dataclasses.dataclass
+from pandas import DataFrame
+
+tire_pos_labels = ['FL', 'FR', 'RL', 'RR']
+FL = 0
+FR = 1
+RL = 2
+RR = 3
+
+
+@dataclasses.dataclass
+class AccSetup:
+    file_path: str
+    params: DataFrame
+
+#     track: str
+#     tire_compound: int
+#     tire_pressures: List[int]
+#     pass
+
+
+def read_setup(setup_file) -> AccSetup:
+    with open(setup_file, 'rt') as f:
+        setup_params_raw = json.load(f)
+
+    return AccSetup(setup_file, parse_setup_params(setup_params_raw))
+
+
+def parse_setup_params(setup_params_raw: Dict) -> DataFrame:
+    setup_params = {}
+    setup_params.update(
+            **build_diffs_by_tire_pos('psi', extract_path(setup_params_raw, 'basicSetup', 'tyres', 'tyrePressure')),
+            **build_diffs_by_tire_pos('toe', extract_path(setup_params_raw, 'basicSetup', 'alignment', 'toe')))
+
+    return pd.DataFrame.from_dict(setup_params, columns=['values'], orient='index')
+
+
+def extract_path(d, *path):
+    for n in path:
+        if n not in d:
+            return None
+        d = d[n]
+    return d
+
+
+def build_diffs_by_tire_pos(name, vals_by_tire_pos: List) -> Dict:
+    if vals_by_tire_pos is None:
+        return {}
+    return {f"{name} {tire_pos_labels[i]}": vals_by_tire_pos[i]
+            for i in range(len(tire_pos_labels))}
+
+
+def compare_setup_params(setup_a: DataFrame, setup_b: DataFrame) -> DataFrame:
+    cmp = pd.concat([setup_a, setup_b], axis=1)
+    return cmp
+
+
+def compare_setups(setup_a: AccSetup, setup_b: AccSetup):
+    cmp = compare_setup_params(setup_a.params, setup_b.params)
+    cmp['delta'] = cmp.iloc[:, 1] - cmp.iloc[:, 0]
+    cmp.columns = [setup_file_short_name(setup_a),
+                   setup_file_short_name(setup_b),
+                   'delta']
+    return cmp
+
+
+def setup_file_short_name(setup_a):
+    return os.path.splitext(os.path.basename(setup_a.file_path))[0]
+
+
+def keep_only_deltas(cmp: DataFrame):
+    return cmp[cmp.delta != 0]
+
+
+if __name__ == '__main__':
+    setup_a_file, setup_b_file = sys.argv[1:3]
+    print(keep_only_deltas(compare_setups(read_setup(setup_a_file), read_setup(setup_b_file))))
---
+++ test_main.py
@@ -0,0 +1,61 @@
+from main import extract_path, build_diffs_by_tire_pos, parse_setup_params, compare_setup_params, AccSetup, \
+    compare_setups
+
+
+def test_extract_path():
+    assert extract_path({'a': {'b': 1}}, 'a', 'b') == 1
+
+
+def test_build_diffs_by_tire_pos():
+    result = build_diffs_by_tire_pos("setup_param_name", [0, 1, 2, 3])
+
+    assert result == {'setup_param_name FL': 0,
+                      'setup_param_name FR': 1,
+                      'setup_param_name RL': 2,
+                      'setup_param_name RR': 3}
+
+
+def test_parse_setup():
+    setup_params_raw = \
+        {
+            "carName": "mclaren_720s_gt3",
+            "basicSetup":
+                {
+                    "tyres":
+                        {
+                            "tyreCompound": 0,
+                            "tyrePressure": [50, 57, 47, 57]
+                        },
+                    "alignment":
+                        {
+                            "camber": [0, 0, 0, 0],
+                            "toe": [36, 36, 20, 20],
+                        }
+                }
+        }
+
+    setup = parse_setup_params(setup_params_raw)
+
+    assert setup.to_dict()['values'] == \
+           {'psi FL': 50,
+            'psi FR': 57,
+            'psi RL': 47,
+            'psi RR': 57,
+            'toe FL': 36,
+            'toe FR': 36,
+            'toe RL': 20,
+            'toe RR': 20}
+
+
+def test_compare_setup_params():
+    setup_a = AccSetup('a', parse_setup_params({"basicSetup": {"tyres": {"tyrePressure": [50, 57, 47, 57]}}}))
+    setup_b = AccSetup('b', parse_setup_params({"basicSetup": {"tyres": {"tyrePressure": [51, 58, 48, 58]}}}))
+
+    result = compare_setups(setup_a, setup_b)
+
+    assert result.to_dict(orient='index') == \
+           {'psi FL': {'a': 50, 'b': 51},
+            'psi FR': {'a': 57, 'b': 58},
+            'psi RL': {'a': 47, 'b': 48},
+            'psi RR': {'a': 57, 'b': 58}}
+
