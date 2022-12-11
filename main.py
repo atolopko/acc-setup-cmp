3bdbfde06f7e8cd0c9a4b3daf57d3a1120bb9a0d
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-12-11 14:47:10 -0500

    add all setup attributes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from typing import List, Dict
 
 import numpy as np
 import pandas as pd
-# @dataclasses.dataclass
 from pandas import DataFrame
 
 tire_pos_labels = ['FL', 'FR', 'RL', 'RR']
@@ -36,29 +35,56 @@ def read_setup(setup_file) -> AccSetup:
 
 def parse_setup_params(setup_params_raw: Dict) -> DataFrame:
     setup_params = {
-        **build_diffs_by_tire_pos('psi', extract_path(setup_params_raw, 'basicSetup', 'tyres', 'tyrePressure')),
-        **build_diffs_by_tire_pos('toe', extract_path(setup_params_raw, 'basicSetup', 'alignment', 'toe')),
+        **build_diffs_by_tire_pos('PSI', extract_path(setup_params_raw, 'basicSetup', 'tyres', 'tyrePressure')),
+        **build_diffs_by_tire_pos('Toe', extract_path(setup_params_raw, 'basicSetup', 'alignment', 'toe')),
+        **build_diffs_by_tire_pos('Camber', extract_path(setup_params_raw, 'basicSetup', 'alignment', 'camber')),
+        'Caster L': extract_path(setup_params_raw, 'basicSetup', 'alignment', 'casterLF'),
+        'Caster R': extract_path(setup_params_raw, 'basicSetup', 'alignment', 'casterRF'),
+        'Steer Ratio': extract_path(setup_params_raw, 'basicSetup', 'alignment', 'steerRatio'),
+        'TC 1': extract_path(setup_params_raw, 'basicSetup', 'electronics', 'tC1'),
+        'TC 2': extract_path(setup_params_raw, 'basicSetup', 'electronics', 'tC2'),
+        'ABS': extract_path(setup_params_raw, 'basicSetup', 'electronics', 'abs'),
+        'Eng Map': extract_path(setup_params_raw, 'basicSetup', 'electronics', 'eCUMap'),
+        'Fuel': extract_path(setup_params_raw, 'basicSetup', 'strategy', 'fuel'),
+        'Bk Cmp F': extract_path(setup_params_raw, 'basicSetup', 'strategy', 'frontBrakePadCompound'),
+        'Bk Cmp R': extract_path(setup_params_raw, 'basicSetup', 'strategy', 'rearBrakePadCompound'),
+
         'ARB F': extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance', 'aRBFront'),
         'ARB R': extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance', 'aRBRear'),
-        **build_diffs_by_tire_pos('wheelRate', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
-                                                            'wheelRate')),
-        **build_diffs_by_tire_pos('bumpUp', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
-                                                            'bumpStopRateUp')),
-        **build_diffs_by_tire_pos('bumpDn', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
-                                                            'bumpStopRateDn')),
-        **build_diffs_by_tire_pos('bumpRng', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
-                                                            'bumpStopWindow')),
+        **build_diffs_by_tire_pos('Spring Rate', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
+                                                              'wheelRate')),
+        **build_diffs_by_tire_pos('Bump Up', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
+                                                          'bumpStopRateUp')),
+        **build_diffs_by_tire_pos('Bump Dn', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
+                                                          'bumpStopRateDn')),
+        **build_diffs_by_tire_pos('Bump Rng', extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance',
+                                                           'bumpStopWindow')),
         'Break TQ': extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance', 'brakeTorque'),
         'Break Bias': extract_path(setup_params_raw, 'advancedSetup', 'mechanicalBalance', 'brakeBias'),
+        **build_diffs_by_tire_pos('Dampers Bump Slow',
+                                  extract_path(setup_params_raw, 'advancedSetup', 'dampers', 'bumpSlow')),
+        **build_diffs_by_tire_pos('Dampers Rebound Slow',
+                                  extract_path(setup_params_raw, 'advancedSetup', 'dampers', 'bumpSlow')),
+        **build_diffs_by_tire_pos('Dampers Bump Fast',
+                                  extract_path(setup_params_raw, 'advancedSetup', 'dampers', 'bumpFast')),
+        **build_diffs_by_tire_pos('Dampers Rebound Fast',
+                                  extract_path(setup_params_raw, 'advancedSetup', 'dampers', 'bumpFast')),
+        'Ride Height F': extract_path(setup_params_raw, 'advancedSetup', 'aeroBalance', 'rodLength', missing=[np.nan] * 4)[2],
+        'Ride Height R': extract_path(setup_params_raw, 'advancedSetup', 'aeroBalance', 'rodLength', missing=[np.nan] * 4)[0],
+        'R Wing': extract_path(setup_params_raw, 'advancedSetup', 'aeroBalance', 'rearWing'),
+        'F Splitter': extract_path(setup_params_raw, 'advancedSetup', 'aeroBalance', 'splitter'),
+        'Brake Duct F': extract_path(setup_params_raw, 'advancedSetup', 'aeroBalance', 'brakeDuct', missing=[np.nan] * 2)[1],
+        'Brake Duct R': extract_path(setup_params_raw, 'advancedSetup', 'aeroBalance', 'brakeDuct', missing=[np.nan] * 2)[0],
+        'Brake Duct R': extract_path(setup_params_raw, 'advancedSetup', 'drivetrain', 'preload'),
     }
 
     return pd.DataFrame.from_dict(setup_params, columns=['values'], dtype=np.float, orient='index')
 
 
-def extract_path(d, *path):
+def extract_path(d, *path, missing=None):
     for n in path:
         if n not in d:
-            return None
+            return missing
         d = d[n]
     return d
 
--- test_main.py
+++ test_main.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-from numpy import nan
-
 from main import extract_path, build_diffs_by_tire_pos, parse_setup_params, AccSetup, \
     compare_setups
 
@@ -22,63 +18,162 @@ def test_build_diffs_by_tire_pos():
 def test_parse_setup():
     setup_params_raw = \
         {
-            "carName": "mclaren_720.0s_gt3.0",
-            "basicSetup": {
-                    "tyres": {
-                            "tyreCompound": 0.0,
-                            "tyrePressure": [50.0, 57.0, 47.0, 57.0]
+            "carName": "mclaren_720s_gt3",
+            "basicSetup":
+                {
+                    "tyres":
+                        {
+                            "tyreCompound": 0,
+                            "tyrePressure": [56, 62, 58, 64]
                         },
-                    "alignment": {
-                            "camber": [0.0, 0.0, 0.0, 0.0],
-                            "toe": [36.0, 36.0, 20.0, 20.0],
+                    "alignment":
+                        {
+                            "camber": [0, 0, 0, 0],
+                            "toe": [38, 38, 25, 25],
+                            "staticCamber": [-4.6132869720458984, -4.6103549003601074, -4.3367342948913574,
+                                             -4.3318619728088379],
+                            "toeOutLinear": [0.0015907743945717812, 0.0015907747438177466, 0.00050982175162062049,
+                                             0.00050975591875612736],
+                            "casterLF": 11,
+                            "casterRF": 11,
+                            "steerRatio": 2
+                        },
+                    "electronics":
+                        {
+                            "tC1": 3,
+                            "tC2": 6,
+                            "abs": 4,
+                            "eCUMap": 0,
+                            "fuelMix": 0,
+                            "telemetryLaps": 0
+                        },
+                    "strategy":
+                        {
+                            "fuel": 20,
+                            "nPitStops": 0,
+                            "tyreSet": 0,
+                            "frontBrakePadCompound": 0,
+                            "rearBrakePadCompound": 0,
+                            "pitStrategy": [
+                                {
+                                    "fuelToAdd": 0,
+                                    "tyres":
+                                        {
+                                            "tyreCompound": 0,
+                                            "tyrePressure": [52, 59, 52, 59]
+                                        },
+                                    "tyreSet": 2,
+                                    "frontBrakePadCompound": 1,
+                                    "rearBrakePadCompound": 1
+                                }
+                            ],
+                            "fuelPerLap": 2.6084213256835938
                         }
                 },
-            "advancedSetup": {
-                    "mechanicalBalance": {
-                            "aRBFront": 4.0,
-                            "aRBRear": 6.0,
-                            "wheelRate": [3.0, 3.0, 5.0, 5.0],
-                            "bumpStopRateUp": [5.0, 5.0, 2.0, 2.0],
-                            "bumpStopRateDn": [0.0, 0.0, 10.0, 10.0],
-                            "bumpStopWindow": [3.0, 3.0, 25.0, 25.0],
-                            "brakeTorque": 20.0,
-                            "brakeBias": 35.0
+            "advancedSetup":
+                {
+                    "mechanicalBalance":
+                        {
+                            "aRBFront": 3,
+                            "aRBRear": 2,
+                            "wheelRate": [3, 3, 4, 4],
+                            "bumpStopRateUp": [5, 5, 2, 2],
+                            "bumpStopRateDn": [0, 0, 10, 10],
+                            "bumpStopWindow": [4, 4, 50, 50],
+                            "brakeTorque": 20,
+                            "brakeBias": 57
+                        },
+                    "dampers":
+                        {
+                            "bumpSlow": [20, 20, 20, 20],
+                            "bumpFast": [23, 23, 30, 30],
+                            "reboundSlow": [8, 8, 22, 22],
+                            "reboundFast": [20, 20, 27, 27]
+                        },
+                    "aeroBalance":
+                        {
+                            "rideHeight": [2, 6, 8, 18],
+                            "rodLength": [58.382854461669922, 58.382854461669922, 37.532012939453125,
+                                          37.532012939453125],
+                            "splitter": 0,
+                            "rearWing": 5,
+                            "brakeDuct": [4, 3]
+                        },
+                    "drivetrain":
+                        {
+                            "preload": 5
                         }
-            }
+                },
+            "trackBopType": 28
         }
 
     setup = parse_setup_params(setup_params_raw)
 
     assert setup.to_dict()['values'] == \
-           {'psi FL': 50.0,
-            'psi FR': 57.0,
-            'psi RL': 47.0,
-            'psi RR': 57.0,
-            'toe FL': 36.0,
-            'toe FR': 36.0,
-            'toe RL': 20.0,
-            'toe RR': 20.0,
-            'ARB F': 4.0,
-            'ARB R': 6.0,
-            'Break Bias': 35.0,
+           {'ABS': 4.0,
+            'ARB F': 3.0,
+            'ARB R': 2.0,
+            'Bk Cmp F': 0.0,
+            'Bk Cmp R': 0.0,
+            'Brake Duct F': 3.0,
+            'Brake Duct R': 5.0,
+            'Break Bias': 57.0,
             'Break TQ': 20.0,
-            'bumpDn FL': 0.0,
-            'bumpDn FR': 0.0,
-            'bumpDn RL': 10.0,
-            'bumpDn RR': 10.0,
-            'bumpRng FL': 3.0,
-            'bumpRng FR': 3.0,
-            'bumpRng RL': 25.0,
-            'bumpRng RR': 25.0,
-            'bumpUp FL': 5.0,
-            'bumpUp FR': 5.0,
-            'bumpUp RL': 2.0,
-            'bumpUp RR': 2.0,
-            'wheelRate FL': 3.0,
-            'wheelRate FR': 3.0,
-            'wheelRate RL': 5.0,
-            'wheelRate RR': 5.0,
-            }
+            'Bump Dn FL': 0.0,
+            'Bump Dn FR': 0.0,
+            'Bump Dn RL': 10.0,
+            'Bump Dn RR': 10.0,
+            'Bump Rng FL': 4.0,
+            'Bump Rng FR': 4.0,
+            'Bump Rng RL': 50.0,
+            'Bump Rng RR': 50.0,
+            'Bump Up FL': 5.0,
+            'Bump Up FR': 5.0,
+            'Bump Up RL': 2.0,
+            'Bump Up RR': 2.0,
+            'Camber FL': 0.0,
+            'Camber FR': 0.0,
+            'Camber RL': 0.0,
+            'Camber RR': 0.0,
+            'Caster L': 11.0,
+            'Caster R': 11.0,
+            'Dampers Bump Fast FL': 23.0,
+            'Dampers Bump Fast FR': 23.0,
+            'Dampers Bump Fast RL': 30.0,
+            'Dampers Bump Fast RR': 30.0,
+            'Dampers Bump Slow FL': 20.0,
+            'Dampers Bump Slow FR': 20.0,
+            'Dampers Bump Slow RL': 20.0,
+            'Dampers Bump Slow RR': 20.0,
+            'Dampers Rebound Fast FL': 23.0,
+            'Dampers Rebound Fast FR': 23.0,
+            'Dampers Rebound Fast RL': 30.0,
+            'Dampers Rebound Fast RR': 30.0,
+            'Dampers Rebound Slow FL': 20.0,
+            'Dampers Rebound Slow FR': 20.0,
+            'Dampers Rebound Slow RL': 20.0,
+            'Dampers Rebound Slow RR': 20.0,
+            'Eng Map': 0.0,
+            'F Splitter': 0.0,
+            'Fuel': 20.0,
+            'PSI FL': 56.0,
+            'PSI FR': 62.0,
+            'PSI RL': 58.0,
+            'PSI RR': 64.0,
+            'R Wing': 5.0,
+            'Ride Height F': 37.532012939453125,
+            'Ride Height R': 58.38285446166992,
+            'Spring Rate FL': 3.0,
+            'Spring Rate FR': 3.0,
+            'Spring Rate RL': 4.0,
+            'Spring Rate RR': 4.0,
+            'Steer Ratio': 2.0,
+            'TC 1': 3.0,
+            'TC 2': 6.0,
+            'Toe FL': 38.0,
+            'Toe FR': 38.0,
+            'Toe RL': 25.0,
+            'Toe RR': 25.0}
 
 
 def test_compare_setup_params():
@@ -87,15 +182,11 @@ def test_compare_setup_params():
 
     result = compare_setups(setup_a, setup_b)
 
-    assert result.fillna(-1).to_dict(orient='index') == \
+    assert result.dropna().to_dict(orient='index') == \
            {
-               'psi FL': {'a': 50.0, 'b': 51.0, 'delta': 1.0},
-               'psi FR': {'a': 57.0, 'b': 58.0, 'delta': 1.0},
-               'psi RL': {'a': 47.0, 'b': 48.0, 'delta': 1.0},
-               'psi RR': {'a': 57.0, 'b': 58.0, 'delta': 1.0},
-               'ARB F': {'a': -1, 'b': -1, 'delta': -1},
-               'ARB R': {'a': -1, 'b': -1, 'delta': -1},
-               'Break TQ': {'a': -1, 'b': -1, 'delta': -1},
-               'Break Bias': {'a': -1, 'b': -1, 'delta': -1}
+               'PSI FL': {'a': 50.0, 'b': 51.0, 'delta': 1.0},
+               'PSI FR': {'a': 57.0, 'b': 58.0, 'delta': 1.0},
+               'PSI RL': {'a': 47.0, 'b': 48.0, 'delta': 1.0},
+               'PSI RR': {'a': 57.0, 'b': 58.0, 'delta': 1.0},
            }
 
